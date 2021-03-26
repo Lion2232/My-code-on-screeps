336cd267ee6600cb752bc228b508aa736cc02459
Creator233 <80350614+AnonymStranger@users.noreply.github.com>
2021-03-26 18:26:03 +0400

    Update main.js

--- main.js
+++ main.js
@@ -74,40 +74,4 @@ module.exports.loop = function() {
                 towerTwo.attack(closestHostile);
             }
         }
-        for (var name in Game.creeps) {
-            var creep = Game.creeps[name];
-            if (creep.memory.role == 'harvester') {
-                roleHarvester.run(creep);
-            }
-            if (creep.memory.role == 'upgrader') {
-                roleUpgrader.run(creep);
-            }
-            if (creep.memory.role == 'builder') {
-                roleBuilder.run(creep)
-            }
-            if (creep.memory.role == 'hauler') {
-                roleHauler.run(creep)
-            }
-            if (creep.memory.role == 'attacker') {
-                roleAttacker.run(creep);
-            }
-            if (creep.memory.role == 'assigner') {
-                roleAssigner.run(creep);
-            }
-            if (creep.memory.role == 'repairer') {
-                roleRepairer.run(creep);
-            }
-            if (creep.memory.role == 'claimer') {
-                roleClaimer.run(creep);
-            }
-            if (creep.memory.role == 'guard') {
-                roleGuard.run(creep);
-            }
-            if (creep.memory.role == 'ghealer') {
-                roleGhealer.run(creep);
-            }
-            if (Game.cpu.bucket == 10000) {
-                Game.cpu.generatePixel();
-            }
-        }
 }
